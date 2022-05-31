78c79f3a5dd7dc9b46f2d77c2a1cd7091a34cc97
Khen Cahyo <khencahyo02@gmail.com>
2022-05-31 11:13:04 +0700

    portofolio tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    content: ['index.html'],
+    theme: {
+        container: {
+            center: true,
+            padding: '16px',
+        },
+        extend: {
+            colors: {
+                sky: '#38bdf8',
+                dark: '#1e293b',
+                secondary: '#64748b',
+            },
+            screens: {
+                '2xl': '1420px',
+            },
+        },
+    },
+    plugins: [],
+}
